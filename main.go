c2ab094f9201b21c940f5517d5f15ff8c053ee13
gludivin <ludivine@Corucent.localdomain>
2025-10-27 15:21:45 +0100

    merge feature-url et dev et resolution de problemes

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ func main() {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
 
+	http.HandleFunc("/url", URLHandler)
+
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func URLHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
 }
