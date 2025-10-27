89edae7cd69358d6df1e4197b935e4a3fa5ede8b
gludivin <ludivine@Corucent.localdomain>
2025-10-27 15:27:08 +0100

    rajout fonctionnalités qui permettent d’afficher la page en couleur

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	})
 
 	http.HandleFunc("/url", URLHandler)
+	http.HandleFunc("/color", ColorHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -19,4 +20,8 @@ func main() {
 
 func URLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
+}
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
 }
